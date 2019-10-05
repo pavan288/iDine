1ea11824900ea708edfc8d31d7b714cb79d5ae21
Pavan Powani <pavan.powani@practo.com>
2019-10-05 17:13:08 +0530

    Add new view for Menu Item Row

---
+++ ItemRow.swift
@@ -0,0 +1,16 @@
+//
+//  ItemRow.swift
+//  iDIne
+//
+//  Created by Pavan Powani on 05/10/19.
+//  Copyright © 2019 Pavan Powani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var item: MenuItem
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
