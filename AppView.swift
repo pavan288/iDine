936932bd6af5b46b3849619740dcc7016d0b23cb
Pavan Powani <pavan.powani@practo.com>
2019-10-05 21:20:46 +0530

    Add Tab view in app

---
+++ AppView.swift
@@ -0,0 +1,34 @@
+//
+//  AppView.swift
+//  iDIne
+//
+//  Created by Pavan Powani on 05/10/19.
+//  Copyright © 2019 Pavan Powani. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Image(systemName: "list.dash")
+                    Text("Menu")
+            }
+
+            OrderView()
+                .tabItem {
+                        Image(systemName: "square.and.pencil")
+                        Text("Order")
+                }
+        }
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        AppView().environmentObject(order)
+    }
+}
